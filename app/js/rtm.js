c8ce12c9997f677fb6137e4f61a74f228e6cd035
Matej Kramny <matejkramny@gmail.com>
2014-06-28 00:25:01 +0100

    Much better landing page

--- app/js/rtm.js
+++ app/js/rtm.js
@@ -5,12 +5,28 @@
 	var height = elem.clientHeight;
 	var radius = Math.min(width, height) / 2;
 
+	var chances = [
+		{
+			min: 0,
+			max: 10,
+			probability: 0.8
+		}, {
+			min: 0,
+			max: 50,
+			probability: 0.15
+		}, {
+			min: 0,
+			max: 100,
+			probability: 0.05
+		}
+	]
+
 	var proc = [{
 		type: 'usage',
-		value: 20,
+		value: 0,
 	}, {
 		type: 'free',
-		value: 80
+		value: 100
 	}];
 	var color = d3.scale.category20();
 
@@ -38,30 +54,41 @@
 		.data(pie(proc))
 	.enter().append('path');
 
+	var colors = ['rgb(174,199,232)', '#1e76a5'];
+
 	path
-		.transition()
-		.duration(500)
-		.attr('fill', function (d, i) { return color(i); })
+		.attr('fill', function (d, i) {
+			if (d.data.type == 'free') return colors[0];
+			else return colors[1];
+		})
 		.attr('d', arc)
 		.each(function(d) {
 			this._current = d;
 		});
 
-	var percent = d3.select('.svg-thumbnail').append('h1')
-		.attr('align', 'text-center')
-		.text('20%');
+	var percent = d3.select('.svg-thumbnail h1')
+		.text('0%');
 
 	setInterval(function () {
-		proc[0].value = Math.random() * 50;
-		proc[1].value = 100 - proc[0].value;
+		var prob = Math.random();
+		var maxProb = 0;
+		var chance = null;
+		console.log(prob);
+		for (var i = 0; i < chances.length; i++) {
+			maxProb += chances[i].probability;
+			if (maxProb > prob) {
+				chance = chances[i];
+				break;
+			}
+		}
 
-		//svg.selectAll('path').remove();
+		proc[0].value = Math.random() * (chance.max - chance.min + 1) + chance.min;
+		proc[1].value = 100 - proc[0].value;
 
 		percent.text(proc[0].value.toFixed()+'%');
 
 		path
 			.data(pie(proc))
-		path
 			.transition()
 			.duration(500)
 			.attrTween('d', function (a) {
@@ -71,6 +98,5 @@
 					return arc(i(t));
 				}
 			})
-
 	}, 1000)
 })()
